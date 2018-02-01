affa06ee15528ea43f005ed888c92d1b0d976806
helderdarocha <helder.darocha@gmail.com>
2018-02-01 12:28:49 -0200

    Initial

---
+++ java.javaee7/src/main/java/campusparty/soujava/javaee7/HelloApplication.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2013 Oracle and/or its affiliates. All rights reserved.
+ *
+ * You may not modify, use, reproduce, or distribute this software except in
+ * compliance with  the terms of the License at:
+ * http://java.net/projects/javaeetutorial/pages/BerkeleyLicense
+ */
+package campusparty.soujava.javaee7;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author ievans
+ */
+@ApplicationPath("/")
+public class HelloApplication extends Application {
+
+}
